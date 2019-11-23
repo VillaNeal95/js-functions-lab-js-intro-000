61d0e17e419adc0717138a80d87566f663944abd
Neal Makh <nealmakh@hotmail.co.uk>
2019-11-23 18:35:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,3 +11,7 @@ return `Happy holidays, ${name}!`
 function happyHolidayTo(holiday, name) {
 return `Happy ${holiday}, ${name}!`
 }
+
+function holidayCountdown(holiday, days) {
+return `It's ${days} until ${holiday}!`
+}
